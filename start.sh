4a53bb2eee1cdb20ee525ca157057f98a390348a
AdamJardalii <adam.jardali@lau.edu>
2025-11-30 16:36:18 +0200

    edit

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Navigate to your project folder
 cd CadabraTest.API
+
+# Restore dependencies
 dotnet restore
-dotnet build
+
+# Build the project
+dotnet build --configuration Release
+
+# Run the project on the assigned PORT
 dotnet run --urls=http://0.0.0.0:$PORT
